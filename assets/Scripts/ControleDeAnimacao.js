a7cee44d925d84810a9d43c583cf9e6f99572144
Fabio <fabio.bap88@gmail.com>
2018-03-19 12:53:50 -0300

    movimento de animacao fix, cosseno, seno

--- assets/Scripts/ControleDeAnimacao.js
+++ assets/Scripts/ControleDeAnimacao.js
@@ -2,7 +2,10 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        anguloDiagonal: cc.Float,
         _animacao: cc.Animation,
+        _senoMinimo: cc.Float,
+        _cossenoMinimo: cc.Float,
 
     },
 
@@ -11,28 +14,38 @@ cc.Class({
     onLoad() {
         this._animacao = this.getComponent(cc.Animation);
         this._animacao.play("Parado");
+        this.anguloDiagonal = 20;
+
+        this._senoMinimo = Math.sin(cc.degreesToRadians(this.anguloDiagonal));
+        this._cossenoMinimo = Math.cos(cc.degreesToRadians(90 - this.anguloDiagonal));
     },
     mudaAnimacao(direcao, estado) {
         let proximaAnimacao = estado;
 
-        if (direcao.x > 0) {
-            proximaAnimacao += "Direita";
-        } else if (direcao.x < 0) {
-            proximaAnimacao += "Esquerda";
-        }
+        let angulo = Math.atan2(direcao.y, direcao.x);
 
-        if (direcao.y > 0) {
-            proximaAnimacao += "Cima";
-        } else if (direcao.y < 0) {
-            proximaAnimacao += "Baixo";
+        let cosseno = Math.cos(angulo);
+        cosseno = Math.abs(cosseno);
+
+        let seno = Math.sin(angulo);
+        seno = Math.abs(seno);
+
+        if (cosseno > this._cossenoMinimo) {
+            if (direcao.x > 0) {
+                proximaAnimacao += "Direita";
+            } else if (direcao.x < 0) {
+                proximaAnimacao += "Esquerda";
+            }
+        }
+        if (seno > this._senoMinimo) {
+            if (direcao.y > 0) {
+                proximaAnimacao += "Cima";
+            } else if (direcao.y < 0) {
+                proximaAnimacao += "Baixo";
+            }
         }
         if (!this._animacao.getAnimationState(proximaAnimacao).isPlaying)
             this._animacao.play(proximaAnimacao);
     },
 
-    start() {
-
-    },
-
-    // update (dt) {},
 });
